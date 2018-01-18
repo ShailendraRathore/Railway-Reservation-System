966e458f653819b048e4e4326516914e011a6e43
Shailendra Singh Rathore <ssr28rathore@gmail.com>
2018-01-18 19:18:47 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,23 @@ using namespace std;
 void login_main();
 void access_menu();
 
+//Classes
+class date
+{
+	void get_date()
+	{
+	int mm, dd, yyyy;
+	cout<<"Enter journey date in format: dd/mm/yyyy"<<endl;
+	scanf("%d/%d/%d", &mm, &dd, &yyyy);
+	}
+	String current_time()
+	{
+		time_t _tm =time(NULL );
+		struct tm * curtime = localtime ( &_tm );
+		return asctime(curtime);
+	}
+}
+
 //MAIN PROGRAM
 int main()
 {
